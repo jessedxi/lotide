376b75a7598c8e2240685da51d54cad2d527e204
Jesse <jessedaniel@live.ca>
2021-02-01 21:26:56 +0000

    head function created

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} ===  ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const head = function(array) {
+  if (array.length > 0) {
+    return array[0];
+  } else {
+    return undefined;
+  }
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "LightHouse", "Labs"]), "Hello");
