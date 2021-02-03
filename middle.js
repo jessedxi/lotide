d2dc8aae15d76aeab7a37b07df3427bd424fe673
Jesse <jessedaniel@live.ca>
2021-02-03 19:11:22 +0000

    added onlyCount function

--- middle.js
+++ middle.js
@@ -36,13 +36,13 @@ const middle = function (array) {
     midValue.push(array[halfPoint + 1]);
   }
   return midValue;
-}
+};
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4] 
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 
 assertArraysEqual(middle([1, 2, 3]), [1, 2, 3]);
