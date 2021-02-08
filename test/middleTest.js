138d81f0650da0870ea684e5bf26f7dffd2c85f5
Jesse <jessedaniel@live.ca>
2021-02-08 20:05:07 +0000

    refac'd tail and middle tests using chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,15 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
+//assertArraysEqual(middle([1, 2, 3]), [2]);
+
+describe('#middle', () => {
+  it("should return [2] when passed [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("should return [2, 3] when passed [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,9 +1,15 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;  
 const tail = require('../tail');
 
-let words = ["Howdy", "What", "Up"];
+describe('#tail', () => {
+  it("should return ['What', 'Up'] when passed ['Howdy', 'What', 'Up']", () => {
+    assert.deepEqual(tail(['Howdy', 'What', 'Up']), ['What', 'Up']);
+  })
+})
+
+/*let words = ["Howdy", "What", "Up"];
 let result = tail(words);
 let test = ["What", "Up"];
 assertEqual(words.length, 3);
 assertEqual(result[0], "What");
-assertEqual(result.join(), test.join());
+assertEqual(result.join(), test.join());*/
